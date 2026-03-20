638cb11a02b8b4457e9d77dddfe390c2a4179959
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-03-20 20:39:00 +0600

    add_cartbloc

--- lib/features/cart/ui/cart_screen.dart
+++ lib/features/cart/ui/cart_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_practice_akshit/features/cart/bloc/cart_bloc.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -8,8 +10,18 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  final CartBloc cartBloc = CartBloc();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Cart Screen')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Cart Screen')),
+      body: BlocConsumer<CartBloc, CartState>(
+        bloc: cartBloc,
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Container();
+        },
+      ),
+    );
   }
 }
