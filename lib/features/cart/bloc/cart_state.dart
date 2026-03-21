9cb522d670e1f3c190ae4e126067af59817809e8
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-03-21 21:04:40 +0600

    add_cartevent

--- lib/features/cart/bloc/cart_state.dart
+++ lib/features/cart/bloc/cart_state.dart
@@ -3,4 +3,8 @@ part of 'cart_bloc.dart';
 @immutable
 sealed class CartState {}
 
+abstract class CartActionState extends CartState{}
+
 final class CartInitial extends CartState {}
+
+class CartSuccessState extends CartState{}
